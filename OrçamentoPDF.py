3063f589ae9cd46660b56f78468cfe8e398e6e89
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-08-08 18:43:57 -0300

    Create OrçamentoPDF.py

---
+++ OrçamentoPDF.py
@@ -0,0 +1,9 @@
+projeto = input('Digite a descrição do projeto: ')
+horas_previstas = int(input('Digite a quantidade de horas previstas: '))
+valor_hora = int(input('Digite o valor da hora trabalhada: '))
+prazo = input('Digite o prazo: ')
+
+valor_orçamento = horas_previstas * valor_hora
+
+print(valor_orçamento)
+
