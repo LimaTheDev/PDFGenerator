6a985d459324c6ee0884a8efb27bfabb6ed7fb60
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-08-08 19:46:29 -0300

    Update OrçamentoPDF.py
    Adicionado o template do orçamento e feito alguns ajustes no codigo

--- OrçamentoPDF.py
+++ OrçamentoPDF.py
@@ -11,14 +11,16 @@ print(valor_orçamento)
 
 pdf = FPDF()
 
-pdf.add_page
+pdf.add_page()
 pdf.set_font("Arial")
+pdf.image("template.png", x=0, y=0)
 
 pdf.text(115, 145, projeto)
-pdf.text(115, 160, horas_previstas)
-pdf.text(115, 175, valor_hora)
+pdf.text(115, 160, str(horas_previstas))
+pdf.text(115, 175, str(valor_hora))
 pdf.text(115, 190, prazo)
 pdf.text(115, 205, str(valor_orçamento))
 
 
 pdf.output("Orçamento.pdf")
+print("Orçamento gerado com sucesso! ")
