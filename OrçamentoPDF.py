656f7c8f04c6983de37baef3d0fea90b995022bd
LimaTheDev <131466381+LimaTheDev@users.noreply.github.com>
2023-08-08 19:04:36 -0300

    Update OrçamentoPDF.py
    Adicionado a biblioteca fpdf e feito o set posicional do orçamento

--- OrçamentoPDF.py
+++ OrçamentoPDF.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 projeto = input('Digite a descrição do projeto: ')
 horas_previstas = int(input('Digite a quantidade de horas previstas: '))
 valor_hora = int(input('Digite o valor da hora trabalhada: '))
@@ -7,3 +9,16 @@ valor_orçamento = horas_previstas * valor_hora
 
 print(valor_orçamento)
 
+pdf = FPDF()
+
+pdf.add_page
+pdf.set_font("Arial")
+
+pdf.text(115, 145, projeto)
+pdf.text(115, 160, horas_previstas)
+pdf.text(115, 175, valor_hora)
+pdf.text(115, 190, prazo)
+pdf.text(115, 205, str(valor_orçamento))
+
+
+pdf.output("Orçamento.pdf")
